4906c8a09bfadd9bbce8154cfc49144e767acd84
David Tabarez <davicitotabarez@gmail.com>
2022-11-18 14:07:55 -0600

    Se creao una clase, tambien se crearon variables

--- src/Poliza.java
+++ src/Poliza.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 
 public class Poliza {
 
 	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		byte tipo_auto = 0, años = 0;
+		double total = 0;
+		double cuota = 0;
+		
+		System.out.println("Ingrese el tipo de auto: ");
+		tipo_auto = entrada.nextByte();
 		
 
 	}
